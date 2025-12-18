905b4de31e742e8e53c9ec8d55ffe6a1c43baf40
raphaeltkorp <raphael@tkorp.com>
2025-12-18 22:49:52 +0100

    Fix controller wiki

--- apps/api/src/modules/wiki/wiki.controller.ts
+++ apps/api/src/modules/wiki/wiki.controller.ts
@@ -1,18 +1,27 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
-import { IsArray, IsInt, IsNotEmpty, IsOptional, IsString, Max, Min, MinLength } from "class-validator";
+import {
+	IsArray,
+	IsInt,
+	IsNotEmpty,
+	IsOptional,
+	IsString,
+	Max,
+	Min,
+	MinLength,
+} from "class-validator";
 import { WikiIngestService } from "./wiki.service";
 
 class IngestRequestDto {
 	@IsString()
 	@IsNotEmpty()
 	@MinLength(2)
-		title!: string;
+	title!: string;
 }
 
 class IngestBatchRequestDto {
 	@IsArray()
 	@IsString({ each: true })
-		titles!: string[];
+	titles!: string[];
 }
 
 class IngestRandomRequestDto {
@@ -20,41 +29,41 @@ class IngestRandomRequestDto {
 	@IsInt()
 	@Min(1)
 	@Max(50)
-		count?: number;
+	count?: number;
 }
 
 @Controller("wiki")
 export class WikiController {
-	constructor (private readonly wikiService: WikiIngestService) {}
+	constructor(private readonly wikiService: WikiIngestService) {}
 
 	@Post("ingest")
 	@HttpCode(HttpStatus.CREATED)
-	async ingestOne (@Body() body: IngestRequestDto) {
+	async ingestOne(@Body() body: IngestRequestDto) {
 		return await this.wikiService.ingestTitle(body.title);
 	}
 
 	@Post("ingest/batch")
 	@HttpCode(HttpStatus.CREATED)
-	async ingestBatch (@Body() body: IngestBatchRequestDto) {
+	async ingestBatch(@Body() body: IngestBatchRequestDto) {
 		return await this.wikiService.ingestTitles(body.titles);
 	}
 
 	@Post("ingest/random")
 	@HttpCode(HttpStatus.CREATED)
-	async ingestRandom (@Body() body: IngestRandomRequestDto) {
+	async ingestRandom(@Body() body: IngestRandomRequestDto) {
 		const count = body.count ?? 10;
 		return await this.wikiService.ingestRandom(count);
 	}
 
 	@Post("ingest/top")
 	@HttpCode(HttpStatus.CREATED)
-	async ingestTop () {
+	async ingestTop() {
 		return await this.wikiService.ingestTopPageviews(100);
 	}
 
 	@Post("ingest/featured")
 	@HttpCode(HttpStatus.CREATED)
-	async ingestFeatured () {
+	async ingestFeatured() {
 		return await this.wikiService.ingestFeatured();
 	}
 }
